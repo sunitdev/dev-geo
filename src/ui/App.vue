a9036373f0b7a68039f51516113f446538d21ada
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2020-01-17 01:23:49 +0000

    Added is mobile message

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -1,12 +1,23 @@
 <template>
-    <app-template>
-        <template v-slot:header>
-            <app-nav :routers="routers"></app-nav>
-        </template>
-        <template v-slot:content>
-            <router-view></router-view>
-        </template>
-    </app-template>
+    <div>
+        <div
+            v-if="isMobile()"
+            class="flex flex-col items-center justify-center h-screen"
+        >
+            <h1 class="font-bold text-2xl mb-2">
+                Sorry, Mobile display not supported.
+            </h1>
+            <p>Please open this site on a desktop.</p>
+        </div>
+        <app-template v-if="!isMobile()">
+            <template v-slot:header>
+                <app-nav :routers="routers"></app-nav>
+            </template>
+            <template v-slot:content>
+                <router-view></router-view>
+            </template>
+        </app-template>
+    </div>
 </template>
 
 <script>
@@ -21,6 +32,14 @@ export default {
     components: {
         AppTemplate,
         AppNav
+    },
+
+    methods: {
+        isMobile() {
+            if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                return true
+           } else { return false }
+        }
     },
 
     data: () => ({
